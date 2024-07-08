2f0af950d328ac09f881f1a676067a16a08458b3
Rahul <rahul.mymail1@gmail.com>
2024-07-09 01:05:52 +0530

    Added Player class

--- src/utils/Player.ts
+++ src/utils/Player.ts
@@ -1,1 +1,10 @@
-// player class 
+// player class
+export class Player {
+    constructor(
+      public id: number,
+      public name: string,
+      public health: number,
+      public defence: number,
+      public attack: number
+    ) {}
+}
