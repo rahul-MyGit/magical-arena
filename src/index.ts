9e5cae33362c4a4f2ca137640ac2d1dee266aedd
Rahul <rahul.mymail1@gmail.com>
2024-07-09 01:20:16 +0530

    Added Layout for Arena and main class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 // main execution
+import { InputNameFromUser, InputNumberFromUser } from "./utils/Common";
+
+//TODO: functions to make: addPlayer to arena, start battle, display all member.
+const mainArenaStart = async() => {
+    const name = await InputNameFromUser("Enter the player's name: ");
+    const health = await InputNumberFromUser(`Enter ${name}'s health: `);
+    const attack = await InputNumberFromUser(`Enter ${name}'s attack: `);
+    const defence = await InputNumberFromUser(`Enter ${name}'s strength: `);
+
+    console.log(name , health, attack, defence)
+}
+
+mainArenaStart();
--- src/utils/Arena.ts
+++ src/utils/Arena.ts
@@ -1,1 +1,15 @@
 //arena class and related functions
+import { Player } from "./Player";
+
+export class Arena {
+    totalPlayers: number;
+    Players: Map<number, Player>;
+
+    constructor(){
+        this.totalPlayers = 0;
+        this.Players = new Map();
+        console.log("WELCOME TO DEATH-MATH ARENA !!!");
+    }
+
+    //TODO: addPlayer , deletePlayer, dislayPlayer, Battle.
+}
