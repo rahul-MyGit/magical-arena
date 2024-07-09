77ab3e3926d940db66f61868d92be1cc7d6743b9
Rahul <rahul.mymail1@gmail.com>
2024-07-09 12:56:09 +0530

    Added main function and all functionality

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,60 @@
 // main execution
 import { InputNameFromUser, InputNumberFromUser } from "./utils/Common";
+import { Arena } from "./utils/Arena";
 
-//TODO: functions to make: addPlayer to arena, start battle, display all member.
-const mainArenaStart = async() => {
-    const name = await InputNameFromUser("Enter the player's name: ");
-    const health = await InputNumberFromUser(`Enter ${name}'s health: `);
-    const attack = await InputNumberFromUser(`Enter ${name}'s attack: `);
-    const defence = await InputNumberFromUser(`Enter ${name}'s strength: `);
+const enterPlayerDetails = async () => {
+    const name = await InputNameFromUser("Enter the Player Name: ");
+    const health = await InputNumberFromUser(`Enter the ${name} health: `);
+    const attack = await InputNumberFromUser(`Enter the ${name} attack: `);
+    const defence = await InputNumberFromUser(`Enter the ${name} defence: `);
 
-    console.log(name , health, attack, defence)
+    return {name, health, attack, defence};
 }
+
+const addPlayerHandler = async () => {
+    const {name, health, attack, defence} = await enterPlayerDetails();
+    console.log(name, health, attack, defence);
+    
+}
+
+const startBattleHandler = async () => {
+    if(1 < 2) {
+        console.log("There should be at least 2 Players in the Arena to start the BATTLE. \nPlease add more players.\n");
+        return;
+    }
+
+    const firstId = await InputNumberFromUser("Enter the first player ID: ");
+    const secondId = await InputNumberFromUser("Enter the second player ID: ");
+}
+
+const displayAvailableOptions = () => {
+    console.log("Options: \n\t1> Adding new Player\n\t2> Start BATTLE\n\t3> Stop the game\n");
+    return;
+}
+
+const mainArenaStart = async() => {
+    while(true) {
+        displayAvailableOptions();
+        const response = await InputNumberFromUser("Enter your choice (INTEGER): ");
+
+        switch (response) {
+            case 1:
+                await addPlayerHandler();
+                break;
+            case 2:
+                await startBattleHandler();
+                break;
+            case 3:
+                console.log("Exiting the game... ");
+                process.exit(0);
+            default:
+                console.log("Don't enter invalid options. Plase try again.\n");
+                break;
+        }
+
+        console.log("\n____________________________________________________________________________________________________________________________________________\n\n");
+        
+    }
+};
 
 mainArenaStart();
